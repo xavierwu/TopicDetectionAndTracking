5acb261d8d6f146c229245d0d3e5c403cb07c374
UnfabulousD <dan6024@hotmail.com>
2015-07-19 23:51:16 +0800

    Update Story.h

--- TopicDetectionAndTracking/Story.h
+++ TopicDetectionAndTracking/Story.h
@@ -5,13 +5,24 @@
 
 class Story
 {
+private:
+//	vector<string> words_s; // the remaining words after stemming
+	vector<int> words; // the index of each word of 'words_s', refer to the glossary
+	int topicID = -1;
+	string timeStamp;
+	
 public:
-	vector<string> words_s; // the remaining words after stemming
-	vector<int> words_i; // the index of each word of 'words_s', refer to the glossary
-	int topicId = -1;
-
 	Story();
 	~Story();
+	
+	void setWords(vector<int> &words);
+	vector<int> getWords() const;
+	void setWord(int index); // operation on 'words'
+	int getWord(int index) const; // operation on 'words' 
+	void setTopicID(int topicID);
+	int getTopicID() const;
+	void setTimeStamp(const string &timeStamp);
+	string getTimeStamp() const;
 };
 
 #endif
