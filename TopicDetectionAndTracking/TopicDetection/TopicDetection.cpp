cd19e17d11b50f5f93368553264fcbe497c5a6c9
UnfabulousD <dan6024@hotmail.com>
2015-07-20 16:49:02 +0800

    Update TopicDetection.cpp

--- TopicDetectionAndTracking/TopicDetection/TopicDetection.cpp
+++ TopicDetectionAndTracking/TopicDetection/TopicDetection.cpp
@@ -1,7 +1,22 @@
 #include "TopicDetection.h"
 
-void TopicDetection(vector<Story> &corpus)
-{
+void TopicDetection(vector<Story> &corpus, int &numOfTopics) {
 	// you may need isTwoStoriesSimilar() in 'StoryLinkDetection.h'
 	// TODO: topic detection
+
+	for (std::vector<Story>::iterator iterUncategorized = corpus.begin();
+			iterUncategorized != corpus.end(); iterCategorized++) {
+		for (std::vector<Story>::iterator iterCategorized = corpus.begin;
+				iterCategorized != iterUncategorized; iterCategorized++) {
+
+			/* IF determined as similar, set the uncategorized story id to the id that the categorized has. */
+			/* Create a new topic otherwise. */
+			if (isTwoStoriesSimilar(iterUncategorized, iterCategorized, threshold)) {
+				corpus[iterUncategorized].setTopicID(corpus[iterCategorized].getTopicID);
+			} else {
+				corpus[iterUncategorized].setTopicID(numOfTopics);
+				numOfTopic++;
+			}
+		}
+	}
 }
