02df5889e5de23bbbd6634dc0b018d8505ef52bb
wzt12353202 <296470625@qq.com>
2015-07-22 11:09:47 +0800

    Update Main.cpp
    把字典改成了两个map，并且由于presentation那里也要做相应的修改，所以先注释了

--- TopicDetectionAndTracking/Main.cpp
+++ TopicDetectionAndTracking/Main.cpp
@@ -9,18 +9,19 @@
 int main (int argc, char **argv)
 {
 	vector<Story> corpus;
-	vector<string> glossary;
+	map<int, string> glossaryIntToString;
+	map<string, int> glossaryStringToInt;
 	vector<Story> firstStories;
 
 	string tkn_file = "Dataset/mttkn/20030402_0530_0700_CNA_MAN.mttkn";
 	string bnd_file = "Dataset/mttkn_bnd/20030402_0530_0700_CNA_MAN.mttkn_bnd";
 
-	DataPreprocessing (corpus, glossary, tkn_file, bnd_file);
+	DataPreprocessing (corpus, glossaryIntToString, glossaryStringToInt, tkn_file, bnd_file);
 
 	int numOfTopics = 0;
 	TopicDetection (corpus, numOfTopics);
 	FirstStoryDetection (firstStories, corpus, numOfTopics);
-	Presentation (firstStories, glossary);
+//	Presentation (firstStories, glossary);
 
 	return 0;
 }
