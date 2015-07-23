d9b5321aa52cab465bb3dda0b78db185ea889dc1
xavierwu <wuzw3@mail2.sysu.edu.cn>
2015-07-23 21:39:08 +0800

    Naive version of TopicDetection.

--- TopicDetectionAndTracking/TopicDetection/TopicDetection.cpp
+++ TopicDetectionAndTracking/TopicDetection/TopicDetection.cpp
@@ -4,4 +4,20 @@ void TopicDetection (vector<Story> &corpus, int &numOfTopics)
 {
 	// you may need isTwoStoriesSimilar() in 'StoryLinkDetection.h'
 	// TODO: topic detection
+
+	numOfTopics = 0;
+	for (std::vector<Story>::iterator iterUncategorized = corpus.begin ();
+		 iterUncategorized != corpus.end (); iterUncategorized++) {
+		for (std::vector<Story>::iterator iterCategorized = corpus.begin();
+			 iterCategorized != iterUncategorized; iterCategorized++) {
+			/* IF determined as similar, set the uncategorized story id to the id that the categorized has. */
+			/* Create a new topic otherwise. */
+			if (isTwoStoriesSimilar (*iterUncategorized, *iterCategorized, threshold)) {
+				iterUncategorized->setTopicID (iterCategorized->getTopicID());
+			} else {
+				iterUncategorized->setTopicID (numOfTopics);
+				numOfTopics++;
+			}
+		}
+	}
 }
--- TopicDetectionAndTracking/TopicDetection/TopicDetection.h
+++ TopicDetectionAndTracking/TopicDetection/TopicDetection.h
@@ -6,9 +6,12 @@ and the creation of bins is an unsupervised task. */
 
 #include "../StoryLinkDetection/StoryLinkDetection.h"
 
+/* Threshold for clustering. */
+static double threshold = 0.8;  // TODO: to be modified
+
 /* Cluster stories into topics.
 Input: corpus
 Output: corpus, with 'topicId' set. */
-void TopicDetection (vector<Story> &corpus, int &numOfTopics);
+void TopicDetection (vector<Story> &corpus);
 
 #endif
